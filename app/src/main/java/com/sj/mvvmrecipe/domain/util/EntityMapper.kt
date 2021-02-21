9771c1cb4350c3cf5fc2112eff7f1471bace9f52
Sujoy Manna <sujoymanna007@gmail.com>
2021-02-21 15:04:27 +0530

    model , entities and mapper classes are added

---
+++ app/src/main/java/com/sj/mvvmrecipe/domain/util/EntityMapper.kt
@@ -0,0 +1,6 @@
+package com.sj.mvvmrecipe.domain.util
+
+interface EntityMapper<Entity , DomainModel> {
+    fun mapFromEntity(entity : Entity): DomainModel
+    fun mapToEntity(domainModel: DomainModel): Entity
+}
