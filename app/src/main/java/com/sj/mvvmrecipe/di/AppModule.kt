4e493da6d5cbf7540ed6a826cb94231e77ec25a2
Sujoy Manna <sujoymanna007@gmail.com>
2021-02-26 08:54:41 +0530

    hilt

---
+++ app/src/main/java/com/sj/mvvmrecipe/di/AppModule.kt
@@ -0,0 +1,28 @@
+package com.sj.mvvmrecipe.di
+
+import android.content.Context
+import com.sj.mvvmrecipe.BaseApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context): BaseApplication{
+        return app as BaseApplication
+    }
+
+    @Singleton
+    @Provides
+    fun provideRandomString(): String{
+        return "Hey look a random string!!!!!GNKGNDFK"
+    }
+}
