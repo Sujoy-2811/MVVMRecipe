f8091744d1dcaf812b5aff4aea56c088bb32e4f6
Sujoy Manna <sujoymanna007@gmail.com>
2021-02-27 12:44:42 +0530

    cardView added

--- app/src/main/java/com/sj/mvvmrecipe/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/sj/mvvmrecipe/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -5,34 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
-import com.google.gson.GsonBuilder
-import com.sj.mvvmrecipe.R
-import com.sj.mvvmrecipe.domain.model.Recipe
-import com.sj.mvvmrecipe.network.RecipeService
-import com.sj.mvvmrecipe.network.model.RecipeDtoMapper
+import com.sj.mvvmrecipe.presentation.ui.components.RecipeCard
 import com.sj.mvvmrecipe.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
@@ -55,20 +34,9 @@ class RecipeListFragment: Fragment() {
                     Log.d(TAG, "RECIPE: ${recipe.title}")
                 }
 
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "RecipeList",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(21)
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(
-                        onClick = {
-//                            viewModel.newSearch()
-                        }
-                    ) {
-                        Text(text = "PERFORM SEARCH")
+                LazyColumn{
+                    itemsIndexed( items = recipes){index, recipe ->
+                        RecipeCard(recipe = recipe, onClick = { })
                     }
                 }
             }
